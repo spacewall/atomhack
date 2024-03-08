324dc5fe80175e3de65217206ae1331b80f82656
Alexander Godov <godovav@oiate.ru>
2024-03-08 13:40:52 +0300

    Create models

--- mars_client/models.py
+++ mars_client/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Report(models.Model):
+    name_and_surname = models.CharField(max_length=30, verbose_name='Фамилия, имя отправителя')
+    context = models.TextField(verbose_name='Текст отчёта')
+    release_date = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    delivery_date = ...
+    files = models.FileField(verbose_name='Дополнительные файлы', blank=True, null=True)
+
+
+    class Meta:
+        verbose_name = 'Отчёт'
+        verbose_name_plural = 'Отчёты'
+        ordering = ['release_date']
+
+    
+    def __str__(self) -> str:
+        return self.name
