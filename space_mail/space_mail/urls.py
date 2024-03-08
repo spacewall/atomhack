d7383a0b92cd210f0a47945f4b8f9ff0822caf26
Alexander Godov <godovav@oiate.ru>
2024-03-08 15:13:04 +0300

    Create primitive views

--- space_mail/space_mail/urls.py
+++ space_mail/space_mail/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import mars_client
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', mars_client.views.index),
+    path('new_report/', mars_client.views.new_report, name='new_report'),
+    path('sent_reports/', mars_client.views.sent_reports, name='sent_reports'),
+    path('pending_reports/', mars_client.views.pending_reports, name='pending_reports')
 ]
