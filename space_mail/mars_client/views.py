b3110c63dace8a7f51c14686fee51324517a7f16
assasinfil <nfa57643@gmail.com>
2024-03-08 15:22:58 +0300

    Bugfix

--- space_mail/mars_client/views.py
+++ space_mail/mars_client/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 
 from mars_client.models import Report
 
+
 def new_report(request):
     template = 'new_report/new_report.html'
 
@@ -10,8 +11,10 @@ def new_report(request):
 
     return render(request, template)
 
+
 def index(request):
     return redirect('new_report')
+
 
 def sent_reports(request):
     template = 'sent/sent.html'
@@ -19,6 +22,7 @@ def sent_reports(request):
     context = {'reports': Report.objects.all()}
 
     return render(request, template, context)
+
 
 def pending_reports(request):
     template = 'waiting/waiting.html'
