064ca308760772e88db4ab43c2795bc9fb7733cd
Alexander Godov <godovav@oiate.ru>
2024-03-08 13:47:29 +0300

    Connect database

--- mars_client/models.py
+++ mars_client/models.py
@@ -5,7 +5,7 @@ class Report(models.Model):
     name_and_surname = models.CharField(max_length=30, verbose_name='Фамилия, имя отправителя')
     context = models.TextField(verbose_name='Текст отчёта')
     release_date = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
-    delivery_date = ...
+    send_date = models.DateTimeField(blank=True, verbose_name='Дата отправки')
     files = models.FileField(verbose_name='Дополнительные файлы', blank=True, null=True)
 
 
