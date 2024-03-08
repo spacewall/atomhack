e014d01c62330d418542846d081a936d5b1a5dca
Alexander Godov <godovav@oiate.ru>
2024-03-08 20:59:09 +0300

    Sort templates and static

--- space_mail/mars_client/views.py
+++ space_mail/mars_client/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from mars_client.models import Report
 
 def new_report(request):
-    template = 'new_report/new_report.html'
+    template = 'new_report.html'
 
     name = request.POST.get('name', '')
     text = request.POST.get('text', '')
@@ -14,14 +14,14 @@ def index(request):
     return redirect('new_report')
 
 def sent_reports(request):
-    template = 'sent/sent.html'
+    template = 'sent.html'
 
     context = {'reports': Report.objects.all()}
 
     return render(request, template, context)
 
 def pending_reports(request):
-    template = 'pending/pending.html'
+    template = 'pending.html'
 
     context = {'reports': Report.objects.all()}
 
