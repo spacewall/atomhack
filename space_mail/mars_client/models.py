ec8d0b62182180de3df29ec03294ec39b3345651
Alexander Godov <godovav@oiate.ru>
2024-03-09 10:31:01 +0300

    Button bug fix

--- space_mail/mars_client/models.py
+++ space_mail/mars_client/models.py
@@ -7,7 +7,7 @@ from django_ckeditor_5.fields import CKEditor5Field
 class Report(models.Model):
     name = models.CharField(max_length=40, verbose_name='Название отчёта')
     author = models.CharField(max_length=30, verbose_name='Фамилия, имя отправителя')
-    context = CKEditor5Field(verbose_name='Текст отчёта', config_name='context')
+    context = CKEditor5Field(verbose_name='Текст отчёта', config_name='context', blank=True)
     release_date = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
     send_date = models.DateTimeField(blank=True, verbose_name='Дата отправки', null=True)
     files = models.FileField(verbose_name='Дополнительные файлы', blank=True, null=True)
--- space_mail/space_mail/settings.py
+++ space_mail/space_mail/settings.py
@@ -139,6 +139,7 @@ CKEDITOR_5_CONFIGS = {
     'default': {
         'toolbar': ['heading', '|', 'bold', 'italic', 'link',
                     'bulletedList', 'numberedList', 'blockQuote', 'imageUpload', ],
+        'language': 'ru'
 
     },
     'context': {
