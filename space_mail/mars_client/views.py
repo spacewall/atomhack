3c3713d946bbdb491be32976205ff8142ec6713d
Alexander Godov <godovav@oiate.ru>
2024-03-09 11:30:54 +0300

    Add sorting

--- space_mail/mars_client/views.py
+++ space_mail/mars_client/views.py
@@ -3,6 +3,7 @@ from django.views.generic import CreateView
 
 from mars_client.models import Report
 
+
 class NewReportCreateView(CreateView):
     model = Report
     template_name = 'new_report.html'
@@ -15,13 +16,32 @@ def index(request):
 
 def sent_reports(request):
     template = 'sent.html'
+    sorting = request.GET.get('sort')
+    filtering = request.GET.get('filter')
 
-    context = {'reports': Report.objects.all()}
+    if sorting == 'release_date':
+        reports = Report.objects.order_by('release_date')
+    
+    elif sorting == 'send_date':
+        reports = Report.objects.order_by('send_date')
+
+    else:
+        reports = Report.objects.all()
+
+    context = {'reports': reports}
 
     return render(request, template, context)
 
 def pending_reports(request):
     template = 'pending.html'
+    sorting = request.GET.get('sort')
+    filtering = request.GET.get('filter')
+
+    if sorting == 'release_date':
+        reports = Report.objects.order_by('release_date')
+
+    else:
+        reports = Report.objects.all()
 
     context = {'reports': Report.objects.all()}
 
