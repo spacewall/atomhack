36d17336b070cd99d768d91c7d6adfcc0e33e797
Alexander Godov <godovav@oiate.ru>
2024-03-10 21:01:33 +0300

    Edit an emulator

--- space_emulator.py
+++ space_emulator.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 
 import psycopg2
@@ -7,6 +8,10 @@ config = dotenv_values(".env")
 LOGIN = config["login"]
 PASSWORD = config["password"]
 DATABASE = config["database"]
+
+with open('periods.json') as file:
+    periods = json.load(file)
+
 with psycopg2.connect(database=DATABASE, user=LOGIN, password=PASSWORD) as connection:
     with connection.cursor() as cur:
         cur.execute("""UPDATE mars_client_report SET send_date = %s WHERE id = %s""", (datetime.now(), 1))
